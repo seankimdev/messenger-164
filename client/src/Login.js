68c01a12a0ed701ad2d0ae256da91033279b0427
Sean Kim <seankim0423@gmail.com>
2021-09-08 16:52:43 -0400

    messages added

--- client/src/Login.js
+++ client/src/Login.js
@@ -2,18 +2,50 @@ import React from "react";
 import { Redirect, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import {
+  makeStyles,
   Grid,
   Box,
   Typography,
   Button,
   FormControl,
   TextField,
+  Paper,
 } from "@material-ui/core";
+import Image from "../src/images/bg-img.png";
+
 import { login } from "./store/utils/thunkCreators";
+
+const useStyles = makeStyles((theme) => ({
+  // paperContainer: {
+  //   backgroundImage: `url(${Image})`,
+  //   backgroundRepeat: "no-repeat",
+  //   backgroundSize: "cover",
+  // },
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: "auto",
+    maxWidth: 500,
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  },
+}));
 
 const Login = (props) => {
   const history = useHistory();
   const { user, login } = props;
+
+  const classes = useStyles();
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -28,41 +60,49 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
-          <Button onClick={() => history.push("/register")}>Register</Button>
+    <Paper className={classes.paper}>
+      <Grid container justify="center">
+        <Grid item xs={4}>
+          <Box style={classes.paperContainer} height="100%" alignItems="center">
+            <Typography>Converse with anyone with any language</Typography>
+          </Box>
         </Grid>
-        <form onSubmit={handleLogin}>
-          <Grid>
+
+        <Grid item xs={6}>
+          <Grid container item>
+            <Typography>Need to register?</Typography>
+            <Button onClick={() => history.push("/register")}>Register</Button>
+          </Grid>
+          <form onSubmit={handleLogin}>
             <Grid>
+              <Grid>
+                <FormControl margin="normal" required>
+                  <TextField
+                    aria-label="username"
+                    label="Username"
+                    name="username"
+                    type="text"
+                  />
+                </FormControl>
+              </Grid>
               <FormControl margin="normal" required>
                 <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
+                  label="password"
+                  aria-label="password"
+                  type="password"
+                  name="password"
                 />
               </FormControl>
+              <Grid>
+                <Button type="submit" variant="contained" size="large">
+                  Login
+                </Button>
+              </Grid>
             </Grid>
-            <FormControl margin="normal" required>
-              <TextField
-                label="password"
-                aria-label="password"
-                type="password"
-                name="password"
-              />
-            </FormControl>
-            <Grid>
-              <Button type="submit" variant="contained" size="large">
-                Login
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </Box>
-    </Grid>
+          </form>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 };
 
