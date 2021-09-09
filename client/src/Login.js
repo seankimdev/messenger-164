21b1df52931c63b85eaaf363378bdde458a40cca
Sean Kim <seankim0423@gmail.com>
2021-09-08 23:54:45 -0400

    Login page grid set up

--- client/src/Login.js
+++ client/src/Login.js
@@ -10,34 +10,57 @@ import {
   FormControl,
   TextField,
   Paper,
+  Container,
+  CssBaseline,
 } from "@material-ui/core";
-import Image from "../src/images/bg-img.png";
+import bgImage from "../src/images/bg-img.png";
 
 import { login } from "./store/utils/thunkCreators";
 
 const useStyles = makeStyles((theme) => ({
-  // paperContainer: {
-  //   backgroundImage: `url(${Image})`,
-  //   backgroundRepeat: "no-repeat",
-  //   backgroundSize: "cover",
-  // },
   root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    margin: "auto",
-    maxWidth: 500,
+    height: "100vh",
   },
   image: {
-    width: 128,
-    height: 128,
+    backgroundImage: `linear-gradient(#3a8dff90, #86b9ff90), url(${bgImage})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
   },
-  img: {
-    margin: "auto",
-    display: "block",
-    maxWidth: "100%",
-    maxHeight: "100%",
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+
+  // buttonPadding: {
+  //   padding: "10px 30px",
+  // },
+  // media: {
+  //   // height: 100,
+  //   // paddingTop: "56.25%", // 16:9
+  // },
+  // card: {
+  //   position: "relative",
+  // },
+  overlay: {
+    color: "white",
+    width: "70%",
+    p: {
+      fontSize: "25px",
+    },
   },
 }));
 
@@ -60,20 +83,45 @@ const Login = (props) => {
   }
 
   return (
-    <Paper className={classes.paper}>
-      <Grid container justify="center">
-        <Grid item xs={4}>
-          <Box style={classes.paperContainer} height="100%" alignItems="center">
-            <Typography>Converse with anyone with any language</Typography>
-          </Box>
+    <Grid container component="main" className={classes.root}>
+      {/* <CssBaseline /> */}
+      <Grid
+        container
+        item
+        xs={false}
+        sm={5}
+        className={classes.image}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item className={classes.overlay}>
+          <Typography>Converse with anyone with any language</Typography>
         </Grid>
-
-        <Grid item xs={6}>
-          <Grid container item>
-            <Typography>Need to register?</Typography>
-            <Button onClick={() => history.push("/register")}>Register</Button>
+      </Grid>
+      <Grid item xs={12} sm={7} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Grid
+            container
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="center"
+            spacing={3}
+          >
+            <Grid item>
+              <Typography>Don't have an account?</Typography>
+            </Grid>
+            <Grid item>
+              <Button
+                onClick={() => history.push("/register")}
+                color="primary"
+                variant="outlined"
+                className={classes.buttonPadding}
+              >
+                Create account
+              </Button>
+            </Grid>
           </Grid>
-          <form onSubmit={handleLogin}>
+          <form onSubmit={handleLogin} className={classes.form}>
             <Grid>
               <Grid>
                 <FormControl margin="normal" required>
@@ -94,15 +142,97 @@ const Login = (props) => {
                 />
               </FormControl>
               <Grid>
-                <Button type="submit" variant="contained" size="large">
+                <Button
+                  type="submit"
+                  color="primary"
+                  variant="contained"
+                  className={classes.buttonPadding}
+                >
                   Login
                 </Button>
               </Grid>
             </Grid>
           </form>
-        </Grid>
+        </div>
       </Grid>
-    </Paper>
+    </Grid>
+
+    // <Container>
+    //   <Grid container justify="center">
+    //     <Grid item xs={12} sm={3} justify="center">
+    //       <Card className={classes.card}>
+    //         <CardMedia
+    //           component="img"
+    //           image="../src/images/bg-img.png"
+    //           className={classes.media}
+    //         />
+    //         <Box className={classes.overlay}>
+    //           <Typography>Converse with anyone with any language</Typography>
+    //         </Box>
+    //       </Card>
+    //     </Grid>
+
+    //     <Grid item xs={12} sm={7}>
+    //       <Container p={5}>
+    //         <Grid container item>
+    //           <Grid
+    //             container
+    //             direction="row"
+    //             justifyContent="flex-end"
+    //             alignItems="center"
+    //             spacing={3}
+    //           >
+    //             <Grid item>
+    //               <Typography>Don't have an account?</Typography>
+    //             </Grid>
+    //             <Grid item>
+    //               <Button
+    //                 onClick={() => history.push("/register")}
+    //                 color="primary"
+    //                 variant="outlined"
+    //                 className={classes.buttonPadding}
+    //               >
+    //                 Create account
+    //               </Button>
+    //             </Grid>
+    //           </Grid>
+    //         </Grid>
+    //         <form onSubmit={handleLogin}>
+    //           <Grid>
+    //             <Grid>
+    //               <FormControl margin="normal" required>
+    //                 <TextField
+    //                   aria-label="username"
+    //                   label="Username"
+    //                   name="username"
+    //                   type="text"
+    //                 />
+    //               </FormControl>
+    //             </Grid>
+    //             <FormControl margin="normal" required>
+    //               <TextField
+    //                 label="password"
+    //                 aria-label="password"
+    //                 type="password"
+    //                 name="password"
+    //               />
+    //             </FormControl>
+    //             <Grid>
+    //               <Button
+    //                 type="submit"
+    //                 color="primary"
+    //                 variant="contained"
+    //                 className={classes.buttonPadding}
+    //               >
+    //                 Login
+    //               </Button>
+    //             </Grid>
+    //           </Grid>
+    //         </form>
+    //       </Container>
+    //     </Grid>
+    //   </Grid>
+    // </Container>
   );
 };
 
